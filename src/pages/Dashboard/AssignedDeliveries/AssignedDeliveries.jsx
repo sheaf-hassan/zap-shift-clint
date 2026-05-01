60aa98cbf418e7fb5f1276c0f5a3b09dfad60110
200-hassan <md.arhassan@gmail.com>
2026-05-02 01:51:28 +0600

    Accept parcel and explore

--- src/pages/Dashboard/AssignedDeliveries/AssignedDeliveries.jsx
+++ src/pages/Dashboard/AssignedDeliveries/AssignedDeliveries.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import useAuth from "../../../hooks/useAuth";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const AssignedDeliveries = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
-  const { data: parcels = [] } = useQuery({
+  const { data: parcels = [], refetch } = useQuery({
     queryKey: ["parcels", user.email, "driver_assigned"],
     queryFn: async () => {
       const res = await axiosSecure.get(
@@ -16,6 +17,25 @@ const AssignedDeliveries = () => {
       return res.data;
     },
   });
+
+  const handleAcceptDelivery = (parcel) => {
+    const statusInfo = { deliveryStatus: "rider_arriving" };
+    axiosSecure
+      .patch(`/parcels/${parcel._id}/status`, statusInfo)
+      .then((res) => {
+        if (res.data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `Thank you for accepting.`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+
   return (
     <div className="m-4">
       <h2 className="text-4xl text-black mb-3">
@@ -30,7 +50,7 @@ const AssignedDeliveries = () => {
               <th></th>
               <th>Name</th>
               <th>Confirm</th>
-              <th>Favorite Color</th>
+              <th>Other Actions</th>
             </tr>
           </thead>
           <tbody>
@@ -39,8 +59,21 @@ const AssignedDeliveries = () => {
                 <th>{i + 1}</th>
                 <td>{parcel.parcelName}</td>
                 <td>
-                  <button className="btn btn-primary text-black mr-2">Accept</button>
-                  <button className="btn btn-warning text-black">Reject</button>
+                  {parcel.deliveryStatus === "driver_assigned" ? (
+                    <>
+                      <button
+                        onClick={() => handleAcceptDelivery(parcel)}
+                        className="btn btn-primary text-black mr-2"
+                      >
+                        Accept
+                      </button>
+                      <button className="btn btn-warning text-black">
+                        Reject
+                      </button>
+                    </>
+                  ) : (
+                    <span>Accepted</span>
+                  )}
                 </td>
                 <td>Blue</td>
               </tr>
